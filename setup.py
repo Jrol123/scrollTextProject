5f6d1a1964c231ce09c85c310615134753b670a4
Artemii <angap4@gmail.com>
2024-08-29 20:47:15 +1000

    update for building ?

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""Скрипт Setup.py для проекта по упаковке"""
+from dotenv import load_dotenv
+from setuptools import setup, find_packages
+import json, os
+
+load_dotenv()
+
+
+def read_pipenv_dependencies(fname):
+    """Получаем из Pipfile.lock зависимости по умолчанию"""
+    filepath = os.path.join(os.path.dirname(__file__), fname)
+    with open(filepath) as lockfile:
+        lockjson = json.load(lockfile)
+        return [dependency for dependency in lockjson.get('default')]
+
+
+if __name__ == '__main__':
+    setup(
+        name='ScrollText',
+        version=os.getenv('PACKAGE_VERSION', '0.1.0'),
+        description=os.getenv('PACKAGE_DESCRIPTION', 'ScrollText'),
+        package_dir={'': 'src'},
+        packages=find_packages('src', include=["ScrollText*"]),
+        install_requires=[*read_pipenv_dependencies('Pipfile.lock')],
+        include_package_data=True,
+    )
